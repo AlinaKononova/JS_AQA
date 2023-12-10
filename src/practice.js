da8cf4d31102d1ecaceb1fe607f3fbd3ce7af8d2
AlinaKononova <54644298+AlinaKononova@users.noreply.github.com>
2023-12-10 20:57:33 +0200

    ptactice data types -  hometask 2

---
+++ src/practice.js
@@ -0,0 +1,17 @@
+//Hometask 2: print different types of data to the console
+
+let message = 'Hello!';
+console.log('Data type of message is: ' + typeof message);
+
+let number = 12.345;
+console.log('Data type of number is: ' + typeof number);
+
+let boolean = false;
+console.log('Data type of boolean is: ' + typeof boolean);
+
+let bigint = (9007199254740991n);
+console.log('Data type of bigint is: ' + typeof bigint);
+
+let car = 'Toyota';
+let bool = false;
+console.log('Data type of string * boolean is: ' + typeof car * bool);
