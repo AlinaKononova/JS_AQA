b2667596c7cca251fd1d031a75e25b0cc6b658e2
AlinaKononova <54644298+AlinaKononova@users.noreply.github.com>
2023-12-12 22:20:15 +0200

    hometask 2 - FizzBuzz

---
+++ src/fizzbuzz.js
@@ -0,0 +1,21 @@
+//Hometask 2
+
+function fizzBuzz(n) {
+    let result = '';
+
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 === 0) {
+            result += 'FizzBuzz ';
+        } else if (i % 3 === 0) {
+            result += 'Fizz ';
+        } else if (i % 5 === 0) {
+            result += 'Buzz ';
+        } else {
+            result += i + ' ';
+        }
+    }
+
+    console.log(result);
+}
+
+fizzBuzz(100);
